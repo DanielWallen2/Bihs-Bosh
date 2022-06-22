12e4c0def6a36c13ab8877d0b601e3fe2b9e7c58
Daniel Wallén <daniel.wallen@bahnhof.se>
2022-06-22 16:27:48 +0200

    addEventListener

--- bishbosh.js
+++ bishbosh.js
@@ -1,21 +1,22 @@
 
-// var element = document.getElementById('calculate-button');
-// element.onclick = bishBosh();
+const btn = document.getElementById('calculate-button');
+btn.addEventListener('click', bishBosh)
 
-// function bishBosh(bishTal, boshTal, loopTal) {
 function bishBosh() {
 
-    let bishTal = document.getElementById('bishTal').value;
-    let boshTal = document.getElementById('boshTal').value;
-    let loopTal = document.getElementById('loopTal').value;
+    let bishTal = parseInt(document.getElementById('bishTal').value);
+    let boshTal = parseInt(document.getElementById('boshTal').value);
+    let loopTal = parseInt(document.getElementById('loopTal').value);
+    
+    let err = '';
+    if(bishTal < 0 || boshTal < 0 || loopTal < 0) { err = 'Inga negativa värden tillåts. '; };
+    if(bishTal < 2) { err += 'Bish-nr måste vara minst 2. '; };
+    if(boshTal < 3) { err += 'Bosh-nr måste vara minst 3. '; };
+    if((bishTal >= loopTal) || (boshTal >= loopTal)) { err += 'Bish-nr och Bosh-nr måste vara mindre än Loop-nr. '; };
+    if(!Number.isInteger(bishTal) || !Number.isInteger(boshTal) || !Number.isInteger(loopTal)) { err += 'Endast heltal tillåts.'; };
+    if(err !== '') { document.getElementById('output').innerHTML = err; return; };
 
-    if(bishTal < 0 || boshTal < 0 || loopTal < 0) {
-        document.getElementById('output').innerHTML = 'Inga negativa värden tillåts!';
-        exit;
-    }
-
-    let x = calcBishBosh(bishTal, boshTal, loopTal);
-    // console.log(x);
+    const x = calcBishBosh(bishTal, boshTal, loopTal);
 
     let html = '', del = ', ';
     for(let i = 0; i <= x.length - 1; i++) {
@@ -53,36 +54,3 @@ function calcBishBosh(bishTal, boshTal, loopTal) {
     return output;
 
 }
-
-
-
-// var x = calcBishBosh(3, 4, 100);
-// console.log(x);
-
-
-
-// const output = [100];
-
-// for(let i = 1; i<=100; i++) {
-
-//     let bbResult = '', bish = '', bosh = '';
-
-//     if((i % 4) === 0) { bosh = 'Bosh'; };
-//     if((i % 3) === 0) { bish = 'Bish'; };
-
-//     if(bish === 'Bish') {
-//         bbResult = bish;
-//         if(bosh === 'Bosh') { bbResult = `${bbResult}-`; };
-//     };
-
-//     if(bosh === 'Bosh') {
-//         bbResult = `${bbResult}${bosh}`;
-//     };
-
-//     if(bbResult === '') { bbResult = `${i}`; };
-
-//     output[i] = `${bbResult}`;
-
-//     console.log(output[i]);
-        
-// };
